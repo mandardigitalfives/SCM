6a129623b253fd20f6573601ae730913a8db1bd0
mandardigitalfives <mandar.joshi@digitalfives.com>
2016-12-15 13:08:03 +0530

    socket io fun comment

--- SCM/www/js/trackTruck.js
+++ SCM/www/js/trackTruck.js
@@ -1,45 +1,45 @@
 angular.module('starter.controllers').controller('trackTruckCtrl', ['$scope', '$rootScope', '$http', '$state', '$stateParams', 'store', '$cordovaGeolocation', '$ionicLoading','$ionicPopup', function($scope, $rootScope, $http, $state, $stateParams, store, $cordovaGeolocation, $ionicLoading,$ionicPopup) {
 
-    // var posOptions = { maximumAge:600000, timeout:7000, enableHighAccuracy: false };
-    // $cordovaGeolocation
-    //     .getCurrentPosition(posOptions)
+    var posOptions = { maximumAge:600000, timeout:7000, enableHighAccuracy: false };
+    $cordovaGeolocation
+        .getCurrentPosition(posOptions)
 
-    // .then(function(position) {
-    //     $scope.lat = position.coords.latitude
-    //     $scope.lng = position.coords.longitude
-    //     $scope.positions= {
-    //         lat: $scope.lat,
-    //         lng: $scope.lng
-    //     }
-    //     console.log($scope.positions)
-    //     $ionicLoading.hide();
-    // }, function(err) {
-    //     console.log(err)
-    // });
-
-
-    // var watchOptions = { maximumAge:600000, timeout:7000, enableHighAccuracy: false };
-
-    // var watch = $cordovaGeolocation.watchPosition(watchOptions);
-
-    // watch.then(
-    //     null,
-    //     function(err) {
-    //         console.log(err)
-    //     },
-
-    //     function(position) {
-    //         $scope.lat = position.coords.latitude
-    //         $scope.lng = position.coords.longitude
-    //         $scope.positions = {
-    //             lat: $scope.lat,
-    //             lng: $scope.lng
-    //         }
+    .then(function(position) {
+        $scope.lat = position.coords.latitude
+        $scope.lng = position.coords.longitude
+        $scope.positions= {
+            lat: $scope.lat,
+            lng: $scope.lng
+        }
+        console.log($scope.positions)
+        $ionicLoading.hide();
+    }, function(err) {
+        console.log(err)
+    });
 
 
-    //     });
+    var watchOptions = { maximumAge:600000, timeout:7000, enableHighAccuracy: false };
 
-    // // watch.clearWatch();
+    var watch = $cordovaGeolocation.watchPosition(watchOptions);
+
+    watch.then(
+        null,
+        function(err) {
+            console.log(err)
+        },
+
+        function(position) {
+            $scope.lat = position.coords.latitude
+            $scope.lng = position.coords.longitude
+            $scope.positions = {
+                lat: $scope.lat,
+                lng: $scope.lng
+            }
+
+
+        });
+
+    // watch.clearWatch();
 
 
     $scope.centerOnMe = function() {
@@ -48,25 +48,25 @@ angular.module('starter.controllers').controller('trackTruckCtrl', ['$scope', '$
         });
     };
 
-    $scope.centerOnMe();
-    var socketURL = "http://localhost:9999";
-    var trackTruck = io(socketURL + '/tracktruck');
-    trackTruck.on('tracktruck', function(data) {
-        for (var i = 0; i < data.length; i++) {
-            $scope.lat = data[i].lat;
-            $scope.lng = data[i].lng;
-            $scope.positions = {
-                lat: data[i].lat,
-                lng: data[i].lng
-            }
-        }
-        console.log($scope.positions);
-        var alertPopup = $ionicPopup.alert({
-                title: "Lat -: "+$scope.lat+"<BR>"+'Lng -:  '+$scope.lng,
-                template: "HI....."
-            });
-        $scope.$apply();
-        $ionicLoading.hide();
-        trackTruck.off();
-    });
+    // $scope.centerOnMe();
+    // var socketURL = "http://localhost:9999";
+    // var trackTruck = io(socketURL + '/tracktruck');
+    // trackTruck.on('tracktruck', function(data) {
+    //     for (var i = 0; i < data.length; i++) {
+    //         $scope.lat = data[i].lat;
+    //         $scope.lng = data[i].lng;
+    //         $scope.positions = {
+    //             lat: data[i].lat,
+    //             lng: data[i].lng
+    //         }
+    //     }
+    //     console.log($scope.positions);
+    //     var alertPopup = $ionicPopup.alert({
+    //             title: "Lat -: "+$scope.lat+"<BR>"+'Lng -:  '+$scope.lng,
+    //             template: "HI....."
+    //         });
+    //     $scope.$apply();
+    //     $ionicLoading.hide();
+    //     trackTruck.off();
+    // });
 }]);
