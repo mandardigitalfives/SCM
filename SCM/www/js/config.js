9791c3e36493416c479f7598eee3cdcbfd00dfbd
mandardigitalfives <mandar.joshi@digitalfives.com>
2016-12-20 15:32:32 +0530

    api for login

--- SCM/www/js/config.js
+++ SCM/www/js/config.js
@@ -1,1 +1,2 @@
-var baseURL ="http://digitalfives-apps.org/android_database_Connect/getJobDetailsAgency.php";
+// var baseURL ="http://digitalfives-apps.org/android_database_Connect/getJobDetailsAgency.php";
+var baseURL ="http://localhost:9999/api/"
--- api/userlogin.js
+++ api/userlogin.js
@@ -3,29 +3,30 @@ var http = require('http');
 var CRUD = require('mysql-crud');
 var env = require('./environment');
 var connection = env.Dbconnection;
-var userCRUD = CRUD(connection,'user');
+var userCRUD = CRUD(connection, 'authenticateUser');
 
-exports.login = function(req,res){
-  	  var email = req.body.user_email;
-      var password = req.body.user_password;
-      userCRUD.load({
-        user_email : email,
-        user_password : password
-      }, function (err, val) {
-        var resdata={
-            record:'',
-            status:false,
-            message :'err'
+exports.login = function(req, res) {
+    var email = req.body.email;
+    var password = req.body.password;
+    console.log(email, password);
+    userCRUD.load({
+        UserId: email,
+        Password: password
+    }, function(err, val) {
+        var resdata = {
+            record: '',
+            status: false,
+            message: 'Something get wrong please try after some time.'
         };
-        if(val.length>0){
-	        resdata.record=val;
-	        resdata.status=true;
-	        resdata.message='successfully login welcome ';
-	        res.jsonp(resdata);
-	      }else{
-          resdata.status = false;
-          resdata.message = 'Wrong user name or password';
-          res.jsonp(resdata);
-      }
-	});
+        if (val.length > 0) {
+            resdata.record = val[0];
+            resdata.status = true;
+            resdata.message = 'Successfully login welcome ';
+            res.jsonp(resdata);
+        } else {
+            resdata.status = false;
+            resdata.message = 'Wrong user name or password';
+            res.jsonp(resdata);
+        }
+    });
 }
