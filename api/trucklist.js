a2ac666f97b17f86b661596b743f7e99657b8944
mandardigitalfives <mandar.joshi@digitalfives.com>
2016-12-21 19:49:29 +0530

    ionic material design added and login pagew

--- api/trucklist.js
+++ api/trucklist.js
@@ -1,30 +1,41 @@
 var express = require('express');
 var http = require('http');
 var CRUD = require('mysql-crud');
-var env =require('./environment');
+var env = require('./environment');
 var connection = env.Dbconnection;
 var trucklistCRUD = CRUD(connection, 'authenticateUser');
 
-exports.getTrucklist = function(req ,res) {
-	console.log(req.body);
+exports.getTrucklist = function(req, res) {
+    console.log("req.body", req.body);
     trucklistCRUD.load({
-      'refUid':req.body.refuid,
-      'type' : req.body.type
-     },function(error, result) {
-       if (result) {
-         responsedata = {
-           status: true,
-           record: result,
-           message: 'Todos List'
-         }
-         res.jsonp(responsedata);
-       } else {
-         responsedata = {
-           status: false,
-           record: result,
-           message: 'Something went wrong.'
-         }
-         res.jsonp(responsedata);
-       }
-  });
+        'refUid': req.body.refuid,
+        'type': req.body.type
+    }, function(error, result) {
+        console.log('result : ', result.length);
+        console.log("error : ", error);
+        if (error) {
+            responsedata = {
+                status: false,
+                record: result,
+                message: 'Something went wrong.'
+            }
+            res.jsonp(responsedata);
+        } else {
+            if (result.length > 0) {
+                responsedata = {
+                    status: true,
+                    record: result,
+                    message: ''
+                }
+                res.jsonp(responsedata);
+            } else {
+                responsedata = {
+                    status: false,
+                    record: '',
+                    message: 'No Record Found'
+                }
+                res.jsonp(responsedata);
+            }
+        }
+    });
 }
