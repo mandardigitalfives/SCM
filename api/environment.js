c49a61d73100c9f0f7b9045b3dc749f7aa7136a3
mandardigitalfives <mandar.joshi@digitalfives.com>
2017-01-10 18:59:00 +0530

    Host changed

--- api/environment.js
+++ api/environment.js
@@ -10,14 +10,6 @@ var enviroment = {
 		    host :'evolveworx.com',
 	}),
 	
-	// var enviroment = {
-	// Dbconnection : mysql.createPool({
-	// 		database : 'scm_db_ver1',
-	// 	    user : 'root',
-	// 		password : '',
-	// 	    host :'localhost',
-	// }),
-
 	/** Function For Time stamp**/
 	timestamp: function() {
       var UTCtimestamp = new Date();
